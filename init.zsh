c05f8bf51d8f8f75b0e1663bfb078a4d99f5636d
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-04-01 11:03:45 -0400

    fix(prompt): split p6_return_words word and variable into separate args

--- init.zsh
+++ init.zsh
@@ -13,64 +13,15 @@ p6df::modules::intacct::deps() {
 ######################################################################
 #<
 #
-# Function: str str = p6df::modules::intacct::prompt::mod()
+# Function: words intacct $INTACCT_API_KEY = p6df::modules::intacct::profile::mod()
 #
 #  Returns:
-#	str - str
+#	words - intacct $INTACCT_API_KEY
 #
-#  Environment:	 P6_DFZ_PROFILE_INTACCT
+#  Environment:	 INTACCT_API_KEY
 #>
 ######################################################################
-p6df::modules::intacct::prompt::mod() {
-  local str=""
-  local profile="$P6_DFZ_PROFILE_INTACCT"
+p6df::modules::intacct::profile::mod() {
 
-  if p6_string_blank_NOT "$profile"; then
-    str="intacct:\t  ${profile}"
-  fi
-
-  p6_return_str "$str"
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::intacct::profile::on(profile, code)
-#
-#  Args:
-#	profile -
-#	code - shell code block
-#
-#  Environment:	 P6_DFZ_PROFILE_INTACCT
-#>
-######################################################################
-p6df::modules::intacct::profile::on() {
-  local profile="$1"
-  local code="$2"
-
-  p6_run_code "$code"
-
-  p6_env_export "P6_DFZ_PROFILE_INTACCT" "$profile"
-
-  p6_return_void
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::intacct::profile::off(code)
-#
-#  Args:
-#	code - shell code block previously passed to profile::on
-#
-#  Environment:	 P6_DFZ_PROFILE_INTACCT
-#>
-######################################################################
-p6df::modules::intacct::profile::off() {
-  local code="$1"
-
-  p6_env_unset_from_code "$code"
-  p6_env_export_un P6_DFZ_PROFILE_INTACCT
-
-  p6_return_void
+  p6_return_words 'intacct' '$INTACCT_API_KEY'
 }
